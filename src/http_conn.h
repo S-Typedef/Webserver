b66b40e5d844a94b6a9bc05fdd2df4dbecbe9956
Typedef <1450393960@qq.com>
2021-08-20 23:38:09 +0800

    fix(build): rewrite makefile

--- src/http_conn.h
+++ src/http_conn.h
@@ -18,16 +18,17 @@
 #include <sys/mman.h>
 #include <stdarg.h>
 #include <errno.h>
+#include <sys/uio.h>
 #include "locker.h"
 
 class http_conn
 {
 public:
-    
+
     static const int FILENAME_LEN = 200;//文件名的最大长度
-    
+
     static const int READ_BUFFER_SIZE = 2048;//缓冲区的大小
-    
+
     static const int WRITE_BUFFER_SIZE = 1024;//写缓冲区的大小
 
 
@@ -80,23 +81,23 @@ public:
     ~http_conn(){}
 
 public:
-    
+
     void init(int sockfd, const sockaddr_in& addr);//初始化新的链接
-   
+
     void close_conn(bool real_close = true); //关闭链接
-    
+
     void process();//处理客户请求
-    
+
     bool read();//非阻塞读操作
-    
+
     bool write();//非阻塞写操作
 
 private:
-   
+
     void init(); //初始化链接
-    
+
     HTTP_CODE process_read();//解析http请求
-    
+
     bool process_write(HTTP_CODE ret);//填充http应答
 
     //下面这一组函数被process调用以分析HTTP请求
@@ -121,55 +122,55 @@ private:
     bool add_blank_line();
 
 public:
-    
+
     static int m_epollfd;//所有socket上的时间都被注册到同一个epoll内核事件表中，所以将epoll文件描述符设置为静态的
-    
+
     static int m_user_count;//统计用户数量
 
 
 private:
-    
+
     int m_sockfd;//该HTTP链接的socket和对方的socket地址
 
     struct sockaddr_in m_address;
 
-    
+
     char m_read_buf[READ_BUFFER_SIZE];//读缓冲区
-    
+
     int m_read_idx;//标识度缓冲区已经读入的客户数据的最后一个字节的下一个位置
-    
+
     int m_checked_idx;//当前正在分析的字符在读缓冲区的位置
-    
+
     int m_start_line;//当前正在解析的行的起始位置
-   
+
     char m_write_buf[WRITE_BUFFER_SIZE]; //写缓冲区
-  
+
     int m_write_idx;  //写缓冲区待发送的字节
 
-    
+
     CHECK_STATE m_checked_state;//主状态机当前所处的状态
-   
+
 
 
     METHOD m_method; //请求方法
 
     char m_real_file[FILENAME_LEN];//客户请求目标文件的完整路径，其内容等于doc_root + m_url,doc_root是网站根目录
-   
+
     char * m_url; //客户请求的目标文件的文件名
-    
+
     char * m_version;//HTTP协议版本号，我们仅支持HTTP1.1
-    
+
     char * m_host;//主机名
-    
+
     int m_content_length;//http请求的消息体的长度
-    
+
     bool m_linger;//HTTP请求是否要求保持链接
 
-    
+
     char * m_file_address;//客户请求的目标文件被mmap到内存中的起始位置
-   
+
     struct stat m_file_stat; //目标文件的状态。通着这个我们可以判断木变文件是否存在，是否为目录，是否可读，并获得文件大小等信息
-   
+
     struct iovec m_iv[2]; //我们将采用writev来执行写操作，所以定义下面两个成员，其中m_iv_count表示背写内存块的数量
 
     int m_iv_count;
